da40301a0f35b97ede8ef33cf30fb7c4cdbf2c55
Fahim <contact@fkds.fr>
2025-08-09 22:30:50 +0200

    functions at the beggining of the file

--- FS22_modVersionTo79_AllPSVersion.ps1
+++ FS22_modVersionTo79_AllPSVersion.ps1
@@ -1,12 +1,33 @@
 # Description: Script PowerShell pour modifier la version de description de modDesc.xml de 80 à 79 dans les mods de Farming Simulator 22.
-Write-Host "Analyse des mods en cours" -ForegroundColor Cyan
-$startTime = Get-Date
-Get-ModFolder 
-Get-FSVersion
-Add-Type -AssemblyName System.IO.Compression.FileSystem
 
+$startTime = Get-Date
+function Get-ModFolder {
+    $modfolder = [xml](Get-Content "C:\Users\$env:USERNAME\Documents\My Games\FarmingSimulator2022\gameSettings.xml")
+    if ($modfolder.gameSettings.modsDirectoryOverride.active -eq "false") {
+        return "C:\Users\$env:USERNAME\Documents\My Games\FarmingSimulator2022\mods"
+    } else {
+        return $modfolder.gameSettings.modsDirectoryOverride.directory
+    }
+    
+}
+
+function Get-FSVersion {
+    $result = Select-String -path "C:\Users\$env:USERNAME\Documents\My Games\FarmingSimulator2022\log.txt" -Pattern "ModDesc" | Select-Object -First 1
+    if ($result) {
+        return $result.Line.Split(":")[1].Trim()
+    } else {
+        return "79"
+    }
+}
+
+$chemin = Get-ModFolder
+Write-Host "Le dossier des mods est : $chemin" -ForegroundColor Cyan
+$FSVersion = Get-FSVersion
+Write-Host "Version de FS22 : $FSVersion" -ForegroundColor Cyan
+Add-Type -AssemblyName System.IO.Compression.FileSystem
 $psMajor = $PSVersionTable.PSVersion.Major
 
+Write-Host "Analyse des mods en cours" -ForegroundColor Cyan
 Get-ChildItem -Path $chemin -Filter *.zip | ForEach-Object {
     $zipPath = $_.FullName
     $tempDir = Join-Path $env:TEMP ([System.IO.Path]::GetRandomFileName())
@@ -54,26 +75,5 @@ Get-ChildItem -Path $chemin -Filter *.zip | ForEach-Object {
 }
 $endTime = Get-Date
 $duration = $endTime - $startTime
-Write-Host "Temps d'exécution total : $($duration.TotalSeconds) secondes" -ForegroundColor Green
+Write-Host "Temps d'execution total : $($duration.TotalSeconds) secondes" -ForegroundColor Green
 #Start-Sleep -Seconds 5
-
-
-function Get-ModFolder {
-    $modfolder = [xml](Get-Content "C:\Users\$env:USERNAME\Documents\My Games\FarmingSimulator2022\gameSettings.xml")
-
-    if ($modfolder.gameSettings.modsDirectoryOverride.active -eq "false") {
-        <# Action to perform if the condition is true #>
-        $chemin= "C:\Users\$env:USERNAME\Documents\My Games\FarmingSimulator2022\mods"
-    } else {
-        <# Action to perform if the condition is false #>
-        $chemin = $modfolder.gameSettings.modsDirectoryOverride.directory
-    }
-    Write-Host "Chemin des mods : $chemin" -ForegroundColor Cyan
-}
-
-function Get-FSVersion {
-    Select-String -path "C:\Users\$env:USERNAME\Documents\My Games\FarmingSimulator2022\log.txt" -Pattern "ModDesc" | ForEach-Object {
-        $FSVersion = $_.Line.Split(":")[1].Trim()
-        Write-Host "Version de Farming Simulator : $FSVersion" -ForegroundColor Cyan  
-}
-}
